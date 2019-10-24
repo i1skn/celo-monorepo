949ebdf68bcc7008535d1c44e5e3fff1b3362435
Mariano Cortesi <mcortesi@gmail.com>
2019-10-24 14:52:29 -0300

    fix tests

--- packages/cli/src/commands/lockedgold/authorize.test.ts
+++ packages/cli/src/commands/lockedgold/authorize.test.ts
@@ -3,6 +3,8 @@ import { testWithGanache } from '../../test-utils/ganache-test'
 import Authorize from './authorize'
 import Register from './register'
 
+process.env.NO_SYNCCHECK = 'true'
+
 testWithGanache('account:authorize cmd', (web3: Web3) => {
   test('can authorize account', async () => {
     const accounts = await web3.eth.getAccounts()
--- packages/cli/src/commands/lockedgold/register.test.ts
+++ packages/cli/src/commands/lockedgold/register.test.ts
@@ -2,6 +2,8 @@ import Web3 from 'web3'
 import { testWithGanache } from '../../test-utils/ganache-test'
 import Register from './register'
 
+process.env.NO_SYNCCHECK = 'true'
+
 testWithGanache('account:register cmd', (web3: Web3) => {
   test('can register account', async () => {
     const accounts = await web3.eth.getAccounts()
@@ -12,6 +14,6 @@ testWithGanache('account:register cmd', (web3: Web3) => {
   test('fails if from is missing', async () => {
     // const accounts = await web3.eth.getAccounts()
 
-    await expect(Register.run()).rejects.toThrow('Missing required flag')
+    await expect(Register.run([])).rejects.toThrow('Missing required flag')
   })
 })
